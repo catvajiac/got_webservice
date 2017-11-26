c20aaca3a7112262571c99c84106fecea2992a15
catvajiac <cvajiac@nd.edu>
2017-11-26 16:21:39 -0500

    add getters

--- _got_database.py
+++ _got_database.py
@@ -6,28 +6,69 @@ class _got_database:
 
   def load_files(self, dir='.'):
     #self.battles = self._load_file('battles.csv', 2, dir)
-    self.deaths = self._load_file('character-deaths.csv', 0, dir)
-    self.predictions = self._load_file('character-predictions.csv', 5, dir)
+    self.character_info = self._load_file('character-deaths.csv', 0, dir)
+    predictions = self._load_file('character-predictions.csv', 5, dir)
+
+    for character in self.character_info:
+      if character in predictions:
+        self.character_info[character].update(predictions[character])
 
   def _load_file(self, file, key, dir='.'):
     with open('{}/{}'.format(dir, file), 'r') as f:
       data = [line.strip().split(',') for line in f.readlines()]
       labels = [label.lower().replace(' ', '_') for label in data.pop(0)]
 
-      print len(labels), len(data[0])
-
       return {line[key]: {k: v for k, v in zip(labels, line)} for line in data}
 
   def is_dead(self, character):
-    if character not in self.deaths:
+    if character not in self.character_info:
       return -1
 
-    return not self.deaths[character]['death_year']
+    return not self.character_info[character]['death_year']
 
-if __name__ == "__main__":
-  db = _got_database()
-  db.load_files(dir='data')
+  def get_gender(self, character):
+    if character not in self.character_info:
+      return -1
 
-  print db.is_dead('Elwood')
-  print db.is_dead('Jate Blackberry')
-  print db.is_dead('Not a character')
+    return 'Male' if self.character_info[character]['gender'] else 'Female'
+
+  def get_nobility(self, character):
+    if character not in self.character_info:
+      return -1
+
+    return 'Noble' if self.character_info[character]['nobility'] else 'Commoner'
+
+  def get_house(self, character):
+    if character not in self.character_info:
+      return -1
+
+    return self.character_info[character]['allegiances']
+
+  def get_birth_year(self, character):
+    if character not in self.character_info:
+      pass
+      
+
+  def get_death_year(self, character):
+    if character not in self.character_info:
+      return -1
+
+    return self.character_info[character]['death_year']
+
+  def get_death_book(self, character):
+    if character not in self.character_info:
+      return -1
+
+    return self.character_info[character]['death_chapter']
+
+  def get_intro_chapter(self, character):
+    if character not in self.character_info:
+      return -1
+
+    return self.character_info[character]['book_intro_chapter']
+
+  def get_title(self, character):
+    if character not in self.predictions:
+      return ""
+
+    return self.predictions[character]['title']
--- test.py
+++ test.py
@@ -7,10 +7,31 @@ class DatabaseUtils(unittest.TestCase):
   db = _got_database._got_database()
   db.load_files(dir="data")
 
-  def test_dead(self):
+  def test_is_dead(self):
     self.assertEqual(self.db.is_dead('Elwood'), False)
     self.assertEqual(self.db.is_dead('Jate Blackberry'), True)
     self.assertEqual(self.db.is_dead('not a character'), -1)
+
+  def test_get_gender(self):
+    pass
+
+  def test_get_nobility(self):
+    pass
+
+  def test_get_house(self):
+    pass
+
+  def test_get_death_year(self):
+    pass
+
+  def test_get_death_book(self):
+    pass
+
+  def test_get_intro_chapter(self):
+    pass
+
+  def test_get_title(self):
+    pass
 
 if __name__ == '__main__':
   unittest.main()
