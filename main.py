c2e5af612746bdff04423089984d1f18d64e15ff
camicarballo <ccarball@nd.edu>
2017-11-28 23:09:34 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python2.7
+
+import cherrypy
+from _got_database import _got_database
+from character_controller import character_controller
+from house_controller import house_controller
+from dead_controller import dead_controller
+from book_controller import book_controller
+
+#######################
+## CREATE DISPATCHER ##
+#######################
+
+def start_service():
+  dispatcher = cherrypy.dispatch.RoutesDispatcher()
+  conf = { 'global' : { 'server.socket_host' : 'localhost', 
+                        'server.socket_port' : 7000
+                      },
+           '/'      : { 'request.dispatch' : dispatcher }
+         }
+  database = _got_database()
+
+  ######################
+  #CHARACTER CONTROLLER#
+  ######################
+
+  char_c = character_controller(database)
+
+  #GET
+  dispatcher.connect('char_get', '/characters/', controller=char_c, action='GET',
+    conditions=dict(method=['GET']))
+
+  #GET (char_name)
+  dispatcher.connect('char_get_char_name', '/characters/:char_name', controller=char_c, 
+    action='GET', conditions=dict(method=['GET']))
+
+  ##################
+  #HOUSE CONTROLLER#
+  ##################
+
+  house_c = house_controller(database)
+
+  #GET
+  dispatcher.connect('house_get', '/houses/', controller=house_c, action='GET',
+    conditions=dict(method=['GET']))
+
+  #GET (house_name)
+  dispatcher.connect('house_get_house_name', '/houses/:house_name', controller=char_c, 
+    action='GET', conditions=dict(method=['GET']))
+
+  #################
+  #DEAD CONTROLLER#
+  #################
+
+  dead_c = dead_controller(database)
+
+  #GET
+  dispatcher.connect('dead_get', '/dead/', controller=dead_c, action='GET',
+    conditions=dict(method=['GET']))
+
+  #GET (book)
+  dispatcher.connect('dead_get_book', '/dead/:book', controller=dead_c, action='GET',
+    conditions=dict(method=['GET']))
+
+  #GET (house_name)
+  dispatcher.connect('dead_get_house_name', '/dead/:house_name', controller=dead_c, action='GET',
+    conditions=dict(method=['GET']))
+
+  #################
+  #BOOK CONTROLLER#
+  #################
+
+  book_c = book_controller(database)
+
+  #GET 
+  dispatcher.connect('book_get', '/bookstats/', controller=book_c, action='GET',
+    conditions=dict(method=['GET']))
+
+  #GET (book)
+  dispatcher.connect('book_get_book', '/characters/', controller=book_c, action='GET',
+    conditions=dict(method=['GET']))
+
+  ##################
+  #START THE SERVER#
+  ##################
+  
+  cherrypy.config.update(conf)
+  app = cherrypy.tree.mount(None, config=conf)
+  cherrypy.quickstart(app)
+
+###################
+## MAIN FUNCTION ##
+###################
+
+if __name__ == "__main__":
+  start_service()
