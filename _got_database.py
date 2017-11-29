307036b70b223f2ca9c6c7d4366de5784377ac85
catvajiac <cvajiac@nd.edu>
2017-11-28 20:23:27 -0500

    add extra methods that we wanted

--- _got_database.py
+++ _got_database.py
@@ -1,91 +1,98 @@
 #!/usr/bin/env python2.7
 
+import csv
+
 class _got_database:
   def __init__(self):
-    pass
+    self.books = {
+      1: 'A Game of Thrones',
+      2: 'A Clash of Kings',
+      3: 'A Storm of Swords',
+      4: 'A Feast for Crows',
+      5: 'A Dance with Dragons'
+    }
 
   def load_files(self, dir='data'):
-    self.character_info = self._load_file('character-deaths.csv', 0, dir)
-    predictions = self._load_file('character-predictions.csv', 5, dir)
+    self.characters = self._load_file('character-deaths.csv', 'name', dir)
+    predictions = self._load_file('character-predictions.csv', 'name', dir)
 
-    for character in self.character_info:
+    for character in self.characters:
       if character in predictions:
-        self.character_info[character].update(predictions[character])
+        self.characters[character].update(predictions[character])
 
     for character in predictions:
-      if character not in self.character_info:
-        self.character_info[character] = predictions[character]
+      if character not in self.characters:
+        self.characters[character] = predictions[character]
 
   def _load_file(self, file, key, dir):
     with open('{}/{}'.format(dir, file), 'r') as f:
-      data = [line.strip().split(',') for line in f.readlines()]
-
-      # csv headers
-      labels = [label.lower().replace(' ', '_') for label in data.pop(0)]
-
-      # selectively turns items into integers if applicable
       ints = lambda x: int(x) if x.isdigit() else x
+      data = [{k: ints(v) for k, v, in line.items()} 
+            for line in map(dict, csv.DictReader(f))]
+      keys = [d.pop(key) for d in data]
 
-      # formats data as {'Arya Stark': {'isalive': 1, ...}}
-      return {line[key]: {k: ints(v) for k, v in zip(labels, line)} for line in data}
+      return {k: v for k, v in zip(keys, data)}
+      
+  def get_characters(self):
+    return self.characters.keys()
 
-  def is_dead(self, character):
-    if character not in self.character_info:
+  def get_houses(self):
+    key = 'allegiances'
+    house = lambda x: x[key] if key in x and x[key] != 'None' else ''
+    return [house(v) for k, v in self.characters.items()]
+
+  def get_field(self, character, field):
+    try:
+      return self.characters[character][field]
+    except:
       return -1
 
-    return not self.character_info[character]['death_year']
+  def is_alive(self, character):
+    return self.get_field(character, 'is_alive')
+
+  def get_dead(self):
+    dead = lambda x: not self.is_alive(x)
+    return filter(dead, self.get_characters())
 
   def get_gender(self, character):
-    if character not in self.character_info:
+    try:
+      return 'Male' if self.characters[character]['gender'] else 'Female'
+    except:
       return -1
 
-    return 'Male' if self.character_info[character]['gender'] else 'Female'
 
   def get_nobility(self, character):
-    if character not in self.character_info:
+    try:
+      return 'Noble' if self.characters[character]['nobility'] else 'Commoner'
+    except:
       return -1
 
-    return 'Noble' if self.character_info[character]['isnoble'] else 'Commoner'
+  def get_house(self, char):
+    return self.get_field(char, 'allegiances')
 
-  def get_house(self, character):
-    if character not in self.character_info:
-      return -1
+  def get_birth_year(self, char):
+    return self.get_field(char, 'date_of_birth')
 
-    return self.character_info[character]['allegiances'].strip('House ')
+  def get_death_year(self, char):
+    return self.get_field(char, 'death_year')
 
-  def get_birth_year(self, character):
-    if character not in self.character_info:
-      return -1
+  def get_death_book(self, char):
+    return self.get_field(char, 'book_of_death')
 
-    return self.character_info[character]['dateofbirth']
-      
+  def get_intro_book(self, char):
+    return [self.get_field(char, 'book{}'.format(i+1)) for i in range(5)].index(1) + 1
 
-  def get_death_year(self, character):
-    if character not in self.character_info:
-      return -1
+  def get_death_chapter(self, char):
+    return self.get_field(char, 'death_chapter')
 
-    return self.character_info[character]['death_year']
+  def get_intro_chapter(self, char):
+    return self.get_field(char, 'book_intro_chapter')
 
-  def get_death_book(self, character):
-    if character not in self.character_info:
-      return -1
+  def get_title(self, char):
+    return self.get_field(char, 'title')
 
-    return self.character_info[character]['book_of_death']
+  def get_book_deaths(self, book):
+    key = 'book_of_death'
 
-  def get_death_chapter(self, character):
-    if character not in self.character_info:
-      return -1
-
-    return self.character_info[character]['death_chapter']
-
-  def get_intro_chapter(self, character):
-    if character not in self.character_info:
-      return -1
-
-    return self.character_info[character]['book_intro_chapter']
-
-  def get_title(self, character):
-    if character not in self.character_info:
-      return -1
-
-    return self.character_info[character]['title']
+    match = lambda x: key in self.characters[x] and self.characters[x][key] == book
+    return filter(match, self.get_dead())
--- test.py
+++ test.py
@@ -7,10 +7,18 @@ class DatabaseUtils(unittest.TestCase):
   db = _got_database._got_database()
   db.load_files(dir="data")
 
-  def test_is_dead(self):
-    self.assertEqual(self.db.is_dead('Elwood'), False)
-    self.assertEqual(self.db.is_dead('Jate Blackberry'), True)
-    self.assertEqual(self.db.is_dead('not a character'), -1)
+  def test_get_houses(self):
+    pass
+
+  def test_is_alive(self):
+    self.assertEqual(self.db.is_alive('Arya Stark'), True)
+    self.assertEqual(self.db.is_alive('Eddard Stark'), False)
+    self.assertEqual(self.db.is_alive('not a character'), -1)
+
+  def test_get_dead(self):
+    self.assertTrue('Jon Snow' not in self.db.get_dead())
+    self.assertTrue('Arya Stark' not in self.db.get_dead())
+    self.assertTrue('Drogo' in self.db.get_dead())
 
   def test_get_gender(self):
     self.assertEqual(self.db.get_gender('Aegon Targaryen'), 'Male')
@@ -27,6 +35,11 @@ class DatabaseUtils(unittest.TestCase):
     self.assertEqual(self.db.get_house('Cersei Lannister'), 'Lannister')
     self.assertEqual(self.db.get_house('not a character'), -1)
 
+  def test_get_birth_year(self):
+    self.assertEqual(self.db.get_birth_year('Arya Stark'), 289)
+    self.assertEqual(self.db.get_birth_year('Drogo'), 267)
+    self.assertEqual(self.db.get_birth_year('not a character'), -1)
+
   def test_get_death_year(self):
     self.assertEqual(self.db.get_death_year('Arya Stark'), '')
     self.assertEqual(self.db.get_death_year('Drogo'), 298)
@@ -36,6 +49,12 @@ class DatabaseUtils(unittest.TestCase):
     self.assertEqual(self.db.get_death_book('Jon Snow'), '')
     self.assertEqual(self.db.get_death_book('Drogo'), 1)
     self.assertEqual(self.db.get_death_book('not a character'), -1)
+
+  def test_get_intro_book(self):
+    self.assertEqual(self.db.get_intro_book('Arya Stark'), 1)
+    self.assertEqual(self.db.get_intro_book('Ramsay Snow'), 2)
+    self.assertEqual(self.db.get_intro_book('Eddard Stark'), 1)
+
 
   def test_get_death_chapter(self):
     self.assertEqual(self.db.get_death_chapter('Jon Snow'), '')
@@ -51,6 +70,11 @@ class DatabaseUtils(unittest.TestCase):
     self.assertEqual(self.db.get_title('Quentin Tyrell'), "Ser")
     self.assertEqual(self.db.get_title('Margaery Tyrell'), "Seven Kingdoms")
     self.assertEqual(self.db.get_title('not a character'), -1)
+
+  def test_get_book_deaths(self):
+    self.assertTrue('Eddard Stark' in self.db.get_book_deaths(1))
+    self.assertTrue('Joffrey Baratheon' in self.db.get_book_deaths(3))
+    self.assertTrue('Arya Stark' not in self.db.get_book_deaths(4))
 
 if __name__ == '__main__':
   unittest.main()
